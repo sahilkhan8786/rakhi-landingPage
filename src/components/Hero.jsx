76fa07e11a7dcd1e01400a7efdd324a08b11af60
sahilkhan8786 <official.khansahil8786@gmail.com>
2024-08-11 15:29:59 +0530

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,17 +32,17 @@ export default function Hero() {
                 </div>
                 <div className='w-full flex justify-center gap-5 max-w-screen-sm mx-auto flex-wrap items-center mt-6 sm:mt-6  lg:mt-0'>
                     <Button
-                        value={"Entertainment"}
-                        link={'https://www.freelancer.in/users/l.php?url=https:%2F%2Fwww.giftanapp.com%2Fcategory%2Fentertainment&sig=fdacb7535bfc7c968927c8bd737b5ae13e813270d79a4ee5aadc320827a6c117'} />
+                        value={" Gift Entertainment"}
+                        link={'https://www.giftanapp.com/category/entertainment'} />
                     <Button
-                        value={'Health and Fitness'}
-                        link={'https://www.freelancer.in/users/l.php?url=https:%2F%2Fwww.giftanapp.com%2Fsearch%3Fkeyword%3DHealth&sig=9ad339ccd3e21950a82f7108ef96f9eaa04282f6a91b69e88d7c795c0c007538'} />
+                        value={' Gift Health and Fitness'}
+                        link={'https://www.giftanapp.com/search?keyword=Health'} />
                     <Button
-                        value={'Books and Magazines'}
-                        link={'https://www.freelancer.in/users/l.php?url=https:%2F%2Fwww.giftanapp.com%2Fsearch%3Fkeyword%3DBooks&sig=45c1a093155d1a25d117742d0f19b06b2003bff3c94b1ccb3d8bcc6dad750c1d'} />
+                        value={' Gift Books and Magazines'}
+                        link={'https://www.giftanapp.com/search?keyword=Books'} />
                     <Button
-                        value={'Knowledge'}
-                        link={'https://www.freelancer.in/users/l.php?url=https:%2F%2Fwww.giftanapp.com%2Fsearch%3Fkeyword%3DKnowledge&sig=8bc1ca0891a7ea4c35b022c634e601d90cd235e7f190de7d0805801f6209ca9d'} />
+                        value={' Gift Knowledge'}
+                        link={'https://www.giftanapp.com/search?keyword=Knowledge'} />
                 </div>
             </div>
         </div>
