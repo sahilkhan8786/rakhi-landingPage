b597f18a0f7c5e041f9e14da31cf3dd3cb94bc82
sahilkhan8786 <official.khansahil8786@gmail.com>
2024-08-11 15:18:19 +0530

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ import d_1 from '@/assets/decoration-1.png'
 import Image from 'next/image'
 import image1 from '@/assets/1.png'
 import image2 from '@/assets/2.png'
+import gift from '@/assets/gift.png'
 import Button from './Button'
 export default function Hero() {
     return (
@@ -21,6 +22,7 @@ export default function Hero() {
                 <Image src={heroImage} alt='hero-image' width={500} className='absolute -bottom-24 z-10' />
                 <Image src={d_1} alt='hero-image' width={102} className='absolute -top-6 left-0  sm:right-0 -z-10 sm:z-10' />
                 <Image src={image1} alt="decoration" width={574} className="absolute -bottom-64 -left-20 -z-10 " />
+                <Image src={gift} alt="decoration" width={250} className="absolute top-1/2  -translate-y-1/2   md:top-[100%] md:right-0 -z-10 " />
                 <Image src={image2} alt="decoration" width={574} className="absolute -top-40 -right-40 -z-10 flower opacity-50 lg:opacity-100" />
                 <div className='flex flex-1 items-center gap-3 flex-col'>
                     <p className='flex flex-col gap-2 text-2xl sm:text-3xl uppercases text-center mt-2 sm:mt-0 text-black/70'>
